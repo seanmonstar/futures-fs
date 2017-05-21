ddb14b142e6e407ff92c5e495a27448d2a4fdbf8
Sean McArthur <sean.monstar@gmail.com>
2017-05-21 12:24:03 -0700

    add a test for longer reads

--- tests/smoke.rs
+++ tests/smoke.rs
@@ -23,3 +23,28 @@ fn test_smoke() {
     assert_eq!(data, b"hello world");
     fs.delete(tmp).wait().unwrap();
 }
+
+
+#[test]
+fn test_smoke_long() {
+    let fs = FsPool::default();
+
+    let mut tmp = env::temp_dir();
+    tmp.push("futures-fs-long");
+
+    let mut sink = fs.write(tmp.clone(), Default::default());
+    for i in 0..10 {
+        sink = sink.send(vec![i + 1; 4096].into()).wait().unwrap();
+    }
+
+    let mut data = Vec::new();
+    for chunk in fs.read(tmp.clone()).wait() {
+        data.extend_from_slice(chunk.unwrap().as_ref());
+    }
+
+    assert_eq!(data.len(), 4096 * 10);
+    assert_eq!(&data[..4096], &[1u8; 4096][..]);
+    assert_eq!(&data[4096..8192], &[2u8; 4096][..]);
+
+    fs.delete(tmp).wait().unwrap();
+}
